d1a6982bd8a1f60978a120e1bdf7b3b65e7f3a5e
ChamarakGajaseni <chamarak.g@ku.th>
2022-12-08 23:28:53 +0700

    8_12

--- Classes.py
+++ Classes.py
@@ -12,119 +12,121 @@ def initialize_int_nested_list(num_floor, num_room):
         result.append(list_)
 
     return  result
+
 class Room:
-	def  __init__(self, floor = 1 , room  = 1, customer = "name" , status = "available"):
-		self.__floor = floor
-		self.__room = room
-		self.__status = status
-		self.__customer = customer
 
-	@property
-	def floor(self):
-		return  self.__floor
+    def  __init__(self, floor = 1 , room  = 1, customer = "name" , status = "available"):
+        self.__floor = floor
+        self.__room = room
+        self.__status = status
+        self.__customer = customer
 
-	@property
-	def room(self):
-		return  self.__room
+    @property
+    def floor(self):
+        return  self.__floor
 
-	@property
-	def status(self):
-		return  self.__status
-	@property
-	def customer(self):
-		return  self.__customer
+    @property
+    def room(self):
+        return  self.__room
+
+    @property
+    def status(self):
+        return  self.__status
+    @property
+    def customer(self):
+        return  self.__customer
 
 
 
 class Customer:
-	def  __init__(self,  name = "name" , promotion = {"discount":0} , penalty = {"late" : 0 }, others = {"do_meth" : ["notes", 0]}):
-		self.__name = name
-		self.__promotion = promotion
-		self.__penalty = penalty
-		self.__other = others
+    def  __init__(self,  name = "name" , promotion = {"discount":0} , penalty = {"late" : 0 }, others = {"do_meth" : ["notes", 0]}):
+        self.__name = name
+        self.__promotion = promotion
+        self.__penalty = penalty
+        self.__other = others
 
-	@property
-	def name(self):
-		return self.__name
+    @property
+    def name(self):
+        return self.__name
 
-	@name.setter
-	def name(self,new_nm):
-		if not isinstance(new_nm,str):
-			raise TypeError("name must be alphabets")
+    @name.setter
+    def name(self,new_nm):
+        if not isinstance(new_nm,str):
+            raise TypeError("name must be alphabets")
 
-		self.__name = new_nm
+        self.__name = new_nm
 
-	@property
-	def promotion(self):
-		if not isinstance(self.__promotion , dict):
-			raise TypeError("promotion must be a dictionary")
-		return self.__promotion
+    @property
+    def promotion(self):
+        if not isinstance(self.__promotion , dict):
+            raise TypeError("promotion must be a dictionary")
+        return self.__promotion
 
-	@promotion.setter
-	def promotion(self, new_pro):
-		if not isinstance(self.__promotion , dict):
-			raise TypeError("promotion must be a dictionary")
-		self.__promotion = new_pro
+    @promotion.setter
+    def promotion(self, new_pro):
+        if not isinstance(self.__promotion , dict):
+            raise TypeError("promotion must be a dictionary")
+        self.__promotion = new_pro
 
-	@property
-	def penalty(self):
-		if not isinstance(self.__penalty,dict):
-			raise  TypeError("Penalty must be a dictionary")
-		return  self.__penalty
+    @property
+    def penalty(self):
+        if not isinstance(self.__penalty,dict):
+            raise  TypeError("Penalty must be a dictionary")
+        return  self.__penalty
 
-	@penalty.setter
-	def penalty(self,new_p):
-		if isinstance(new_p,dict):
-			raise  TypeError("Penalty must be a dictionary")
+    @penalty.setter
+    def penalty(self,new_p):
+        if isinstance(new_p,dict):
+            raise  TypeError("Penalty must be a dictionary")
 
-		self.__penalty = new_p
+        self.__penalty = new_p
 
 class Bill:
 
-	"""initialize """
-	def  __init__(self, electric = 0 , water = 0  , room = Room() , customer = Customer()):
-		self.__electric = electric
-		self.__water = water
-		self.__room = room
-		self.__customer = customer
+    """initialize """
+    def  __init__(self, electric = 0 , water = 0  , room = Room() , customer = Customer()):
+        self.__electric = electric
+        self.__water = water
+        self.__room = room
+        self.__customer = customer
 
-	@property
-	def room(self):
-		return  self.__room
+    @property
+    def room(self):
+        return  self.__room
 
-	@property
-	def customer(self):
-		return  self.__customer
+    @property
+    def customer(self):
+        return  self.__customer
 
-	@property
-	def electric(self):
-		return  self.__electric
+    @property
+    def electric(self):
+        return  self.__electric
 
-	@electric.setter
-	def electric(self,new_e):
-		self.__electric = new_e
+    @electric.setter
+    def electric(self,new_e):
+        self.__electric = new_e
 
-	@property
-	def water(self):
-		return  self.__water
+    @property
+    def water(self):
+        return  self.__water
 
-	@water.setter
-	def water(self, new_w):
-		self.__water = new_w
-	def calculate_costs(self,num_floor, num_room):
-		"""the highest floor is 5 which has the cheapest room
-		   cost of 4000 Baht. further more if room number is 3
-		   or 4 they will cost 100 Baht less
-		"""
-		elec_cost = self.electric * 8
-		water_cost = self .water * 18
-		room_cost = 4000 + (5-num_floor)*100
-		if num_room == 3 or num_room == 4:
-			room_cost -= 100
-		else:
-			pass
+    @water.setter
+    def water(self, new_w):
+        self.__water = new_w
+    def calculate_costs(self,num_floor, num_room):
+        """the highest floor is 5 which has the cheapest room
+           cost of 4000 Baht. further more if room number is 3
+           or 4 they will cost 100 Baht less
+        """
+        elec_cost = self.electric * 8
+        water_cost = self .water * 18
+        room_cost = 4000 + (5-num_floor)*100
+        if num_room == 3 or num_room == 4:
+            room_cost -= 100
+        else:
+            pass
 
-		return elec_cost,water_cost,room_cost
+        return elec_cost,water_cost,room_cost
 
 
 
--- DO_BILL.py
+++ DO_BILL.py
@@ -1,2 +1,73 @@
 from  Classes import  Room , Bill , Customer
 
+floors = 5
+room = 4
+init_txt = "9_2022.txt"
+new_txt = "10_2022.txt"
+
+def initialize_nested_list(num_rows, num_cols):
+    list1 = []
+    list2 = []
+
+    for i in range(num_rows):
+        list2.append(list1)
+
+    return list2
+
+def operate():
+    floors = 5
+    room = 4
+    init_txt = "9_2022.txt"
+    new_txt = "10_2022.txt"
+
+    init_lines = open(init_txt).read().splitlines()
+    new_lines = open(new_txt).read().splitlines()
+    init_table = [x.split(",") for x in init_lines if x != ""]
+    new_table = [x.split(",") for x in new_lines if x != ""]
+    inted_init = []
+    inted_new = []
+
+    for row in init_table:
+        _list = []
+        for num in row:
+            _list.append(int(num))
+        inted_init.append(_list)
+
+    for row in new_table:
+        _list = []
+        for num in row:
+            _list.append(int(num))
+        inted_new.append(_list)
+
+    apartment = initialize_nested_list(floors, room)
+
+    for floor in apartment:
+        for room in floor:
+            room.append(inted_init[0])
+
+    print(inted_init)
+    print(inted_new)
+    print(apartment)
+
+
+# meth = input("1.set size: \n 2.set text: \n 3. exit")
+while True:
+    meth = int(input("1.set size: \n2.set text: \n3.show \n0.exit \nenter choice: "))
+    if meth == 0:
+        break
+
+    if meth == 1:
+        floors = int(input("how many floors?: "))
+        room = int(input("how many rooms per floor?: "))
+    elif meth == 2:
+        print("Please insert text file in Rent_Calculator folder")
+        init_txt = input("enter initial month text name(mm_yyyy.txt): ")
+        new_txt = input("enter current month text name(mm_yyyy.txt): ")
+
+    elif meth == 3:
+        operate()
+
+    else:
+        print("incorrect answer try again")
+
+print("closed")
