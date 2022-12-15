fbf6e2ad4d447936f8eb854be47a23c7e5795112
ChamarakGajaseni <chamarak.g@ku.th>
2022-12-15 19:08:57 +0700

    the no turtle graphic version

--- Classes.py
+++ Classes.py
@@ -1,132 +1,230 @@
-def initialize_int_nested_list(num_floor, num_room):
+import copy
+def initialize_nested_list(num_rows, num_cols):
+    empty_tab = []
+    for r in range(num_rows):
+        empty_tab.append([])
 
-    dict_ = {}
-    result = []
-    for num in range(num_floor):
-        dict_[f'col{num+1}'] = num_room
+    for f in range(len(empty_tab)):
+        for r in range(num_cols):
+            empty_tab[f].append(0)
 
-    for item in dict_:
-        list_ = []
-        for num in range(dict_[item]):
-            list_.append(0)
-        result.append(list_)
+    return empty_tab
 
-    return  result
 
 class Room:
 
-    def  __init__(self, floor = 1 , room  = 1, customer = "name" , status = "available"):
+    def __init__(self, floor=1, room=1, water=0, electric=0, status="available", cost=4000):
         self.__floor = floor
         self.__room = room
         self.__status = status
-        self.__customer = customer
+        self.__electric = electric
+        self.__water = water
+        self.cost = cost
 
     @property
     def floor(self):
-        return  self.__floor
+        return self.__floor
 
     @property
     def room(self):
-        return  self.__room
+        return self.__room
+
+    @property
+    def electric(self):
+        return self.__electric
+
+    @electric.setter
+    def electric(self, new_e):
+        self.__electric = new_e
+
+    @property
+    def water(self):
+        return self.__water
+
+    @water.setter
+    def water(self, new_w):
+        self.__water = new_w
 
     @property
     def status(self):
-        return  self.__status
-    @property
-    def customer(self):
-        return  self.__customer
+        return self.__status
 
+    @status.setter
+    def status(self,new_stat):
+        self.__status = new_stat
 
 
 class Customer:
-    def  __init__(self,  name = "name" , promotion = {"discount":0} , penalty = {"late" : 0 }, others = {"do_meth" : ["notes", 0]}):
+    def __init__(self, name="", floor=1, room=1, others={"discount": -0, "penalty": +0, "others(example)": 0}):
         self.__name = name
-        self.__promotion = promotion
-        self.__penalty = penalty
+        self.__floor = floor
+        self.__room = room
         self.__other = others
+
+    def __repr__(self):
+        return f"customer's name : {self.name} room : {self.floor}0{self.room}"
 
     @property
     def name(self):
         return self.__name
 
     @name.setter
-    def name(self,new_nm):
-        if not isinstance(new_nm,str):
+    def name(self, new_nm):
+        if not isinstance(new_nm, str):
             raise TypeError("name must be alphabets")
 
         self.__name = new_nm
 
     @property
-    def promotion(self):
-        if not isinstance(self.__promotion , dict):
-            raise TypeError("promotion must be a dictionary")
-        return self.__promotion
-
-    @promotion.setter
-    def promotion(self, new_pro):
-        if not isinstance(self.__promotion , dict):
-            raise TypeError("promotion must be a dictionary")
-        self.__promotion = new_pro
-
-    @property
-    def penalty(self):
-        if not isinstance(self.__penalty,dict):
-            raise  TypeError("Penalty must be a dictionary")
-        return  self.__penalty
-
-    @penalty.setter
-    def penalty(self,new_p):
-        if isinstance(new_p,dict):
-            raise  TypeError("Penalty must be a dictionary")
-
-        self.__penalty = new_p
-
-class Bill:
-
-    """initialize """
-    def  __init__(self, electric = 0 , water = 0  , room = Room() , customer = Customer()):
-        self.__electric = electric
-        self.__water = water
-        self.__room = room
-        self.__customer = customer
+    def floor(self):
+        return self.__floor
 
     @property
     def room(self):
-        return  self.__room
+        return self.__room
 
     @property
-    def customer(self):
-        return  self.__customer
+    def other(self):
+        if not isinstance(self.__other, dict):
+            raise TypeError("promotion must be a dictionary")
+        return self.__other
+
+    @other.setter
+    def other(self, new):
+        if not isinstance(self.__other, dict):
+            raise TypeError("promotion must be a dictionary")
+        self.__other = new
+
+
+class Bill:
+    """initialize """
+
+    def __init__(self, floor_size, room_size, rooms=[], customers=[], ):
+        self.fs = floor_size
+        self.rs = room_size
+        self.__rooms = rooms
+        self.__customers = customers
+        for room in range(len(self.rooms)):
+            self.rooms[room].status = "unavailable"
 
     @property
-    def electric(self):
-        return  self.__electric
+    def rooms(self):
+        if not isinstance(self.__rooms, list):
+            raise TypeError("rooms must be a list")
+        for room in self.__rooms:
+            if not isinstance(room, Room):
+                raise TypeError("Must be room")
 
-    @electric.setter
-    def electric(self,new_e):
-        self.__electric = new_e
+        return self.__rooms
+
+    @rooms.setter
+    def rooms(self, n_rooms):
+        if not isinstance(n_rooms, list):
+            raise TypeError("rooms must be a list")
+        for room in n_rooms:
+            if not isinstance(room, Room):
+                raise TypeError("Must be room")
+        self.__rooms = n_rooms
 
     @property
-    def water(self):
-        return  self.__water
+    def customers(self):
+        if not isinstance(self.__customers, list):
+            raise TypeError("customers must be a list")
+        for c in self.__customers:
+            if not isinstance(c, Customer):
+                raise TypeError("Must be customer")
 
-    @water.setter
-    def water(self, new_w):
-        self.__water = new_w
-    def calculate_costs(self,num_floor, num_room):
-        """the highest floor is 5 which has the cheapest room
-           cost of 4000 Baht. further more if room number is 3
-           or 4 they will cost 100 Baht less
-        """
-        elec_cost = self.electric * 8
-        water_cost = self .water * 18
-        room_cost = 4000 + (5-num_floor)*100
-        if num_room == 3 or num_room == 4:
-            room_cost -= 100
-        else:
-            pass
+        return self.__customers
 
-        return elec_cost,water_cost,room_cost
+    @customers.setter
+    def customers(self, n_cus):
+        if not isinstance(n_cus, list):
+            raise TypeError("customers must be a list")
+        for c in n_cus:
+            if not isinstance(c, Customer):
+                raise TypeError("Must be customer")
+
+    def get_calculated_costs(self, previous_bill):
+        blank_nested = initialize_nested_list(self.fs, self.rs)
+        water_tab = copy.deepcopy(blank_nested)
+        elec_tab = copy.deepcopy(blank_nested)
+        total_tab = copy.deepcopy(blank_nested)
+        roomc_tab = copy.deepcopy(blank_nested)
+        """ Apparently , according to the law you have to charge people water and electric cost
+        for 18 and 8 Baht per unit accordingly"""
+        for room in range(len(self.rooms)):
+            water_cost = (self.rooms[room].water - previous_bill.rooms[room].water) * 18
+            elec_cost = (self.rooms[room].electric - previous_bill.rooms[room].electric) * 8
+            roomcost = self.rooms[room].cost
+            room_total = water_cost + elec_cost + roomcost
+            water_tab[self.rooms[room].floor - 1][self.rooms[room].room - 1] = water_cost
+            elec_tab[self.rooms[room].floor - 1][self.rooms[room].room - 1] = elec_cost
+            roomc_tab[self.rooms[room].floor - 1][self.rooms[room].room - 1] = roomcost
+            total_tab[self.rooms[room].floor - 1][self.rooms[room].room - 1] = room_total
+
+        return water_tab, elec_tab, roomc_tab, total_tab
+
+    def get_name_table(self):
+        name_tab = initialize_nested_list(self.fs, self.rs)
+        for r in range(len(self.rooms)):
+            if self.rooms[r].status == "available":
+                name_tab[self.rooms[r].floor - 1][self.rooms[r].room - 1] = "available"
+            else:
+                name_tab[self.rooms[r].floor - 1][self.rooms[r].room - 1] = self.customers[r].name
+
+        return name_tab
+
+    def add_customer(self, floor, room, name, cost, waterc, elecc):
+        self.customers.append(Customer(name, floor, room))
+        self.rooms.append(Room(floor, room, waterc, elecc, "unavailable", cost))
+
+    def remove_customer(self, name):
+        fl = 1
+        rm = 1
+        for c in self.customers:
+            if c.name == name:
+                fl = c.floor
+                rm = c.room
+                self.customers.remove(c)
+
+        for r in self.rooms:
+            if r.floor == fl and r.room == rm:
+                self.rooms.remove(r)
+
+    def remove_all(self):
+        for c in self.customers:
+            self.customers.remove(c)
+
+        for r in self.rooms:
+            self.rooms.remove(r)
 
 
+class Vector:
+    def __init__(self, x=0, y=0):
+        self.__x = x
+        self.__y = y
 
+    @property
+    def x(self):
+        if not isinstance(self.__x, (int, float)):
+            raise TypeError("Must be a number")
+        return self.__x
+
+    @x.setter
+    def x(self, newx):
+        if not isinstance(newx, (int, float)):
+            raise TypeError("Must be a number")
+
+        self.__x = newx
+
+    @property
+    def y(self):
+        if not isinstance(self.__y, (int, float)):
+            raise TypeError("Must be a number")
+        return self.__y
+
+    @y.setter
+    def y(self, newy):
+        if not isinstance(newy, (int, float)):
+            raise TypeError("Must be a number")
+        self.__y = newy
--- DO_BILL.py
+++ DO_BILL.py
@@ -1,24 +1,7 @@
 from  Classes import  Room , Bill , Customer
+import turtle
 
-floors = 5
-room = 4
-init_txt = "9_2022.txt"
-new_txt = "10_2022.txt"
-
-def initialize_nested_list(num_rows, num_cols):
-    list1 = []
-    list2 = []
-
-    for i in range(num_rows):
-        list2.append(list1)
-
-    return list2
-
-def operate(init_txt):
-    floors = 5
-    room = 4
-    init_txt = "9_2022.txt"
-    new_txt = "10_2022.txt"
+def get_bills(init_txt,new_txt,floors,rooms):
 
     init_lines = open(init_txt).read().splitlines()
     new_lines = open(new_txt).read().splitlines()
@@ -29,50 +12,149 @@ def operate(init_txt):
 
     for row in init_table:
         _list = []
-        for num in row:
-            _list.append(int(num))
+        for num in range(len(row)-1):
+            _list.append(int(row[num]))
+        _list.append(row[5])
         inted_init.append(_list)
 
     for row in new_table:
         _list = []
-        for num in row:
-            _list.append(int(num))
+        for num in range(len(row)-1):
+            _list.append(int(row[num]))
+        _list.append(row[5])
         inted_new.append(_list)
 
-    apartment = initialize_nested_list(floors, room)
+    currooms = []
+    prerooms = []
+    curcustoms = []
+    precustoms = []
 
-    for floor in apartment:
-        for room in floor:
-            room.append(inted_init[0])
+    for room in range(len(inted_init)):
+        curroom = Room(inted_new[room][0],inted_new[room][1],inted_new[room][2],inted_new[room][3],"unavailable",inted_new[room][4])
+        preroom = Room(inted_init[room][0],inted_init[room][1],inted_init[room][2],inted_init[room][3],"unavailable",inted_init[room][4])
+        curcustomer = Customer(inted_new[room][5],inted_new[room][0],inted_new[room][1])
+        precustomer = Customer(inted_new[room][5],inted_new[room][0],inted_new[room][1])
+        currooms.append(curroom)
+        prerooms.append(preroom)
+        curcustoms.append(curcustomer)
+        precustoms.append(precustomer)
 
-    print(inted_init)
-    print(inted_new)
-    print(apartment)
+    curbill = Bill(floors,rooms,currooms,curcustoms)
+    prebill = Bill(floors,rooms,prerooms,precustoms)
+
+    return curbill,prebill
+
+def draw_table(table,cell_height,cell_width):
+    print('|', end = '')
+    for j in range(cell_width+1):
+        print('-'*9 + '|', end = '')
+    print()
+    print('|'+' '*9, end='|')
+    for j in range(cell_width):
+        print(f'{j+1:^9}|', end='')
+    print()
+    print('|', end = '')
+    for j in range(cell_width+1):
+        print('-'*9 + '|', end = '')
+    print()
+
+    for j in range(cell_height):
+        print('|', end='')
+        print(f'{j + 1:^9}',end = '')
+        print('|',end='')
+        for k in range(cell_width):
+            if table[j][k] == 0:
+                x = " "*9
+            else:
+                x = table[j][k]
+            print(f'{x:^9}|' , end = '')
+        print()
+        print('|',end='')
+        for k in range(cell_width+1):
+            print('-' * 9 + '|', end='')
+        print()
 
 
-# meth = input("1.set size: \n 2.set text: \n 3. exit")
+print("Please insert text file in Rent_Calculator folder")
+print("")
+nmon, nyr = int(input("Enter billing month(answer in number ex. 12): ")), int(input("year (answer in number ex. 2021): "))
+init_txt = f"{nmon - 1}_{nyr}.txt"
+new_txt = f"{nmon}_{nyr}.txt"
+floors = int(input("How many floors? : "))
+rooms = int(input("How many rooms? : "))
+curbill,prebill = get_bills(init_txt,new_txt,floors,rooms)
+water_tab,elec_tab,roomc_tab,total_tab = curbill.get_calculated_costs(prebill)
+custable = curbill.get_name_table()
+print("")
+
 while True:
-    print("Please insert text file in Rent_Calculator folder")
-    init_txt = input("enter initial month text name(mm_yyyy.txt): ")
-    new_txt = input("enter current month text name(mm_yyyy.txt): ")
-    meth = int(input("1.set size: \n2.change data: \n3.show \n0.exit \nenter choice: "))
+    meth = int(input("1.change size \n2.change datas \n3.show all room rent \n4.show all customer \n5.show specific room detail \n6.add customer manually\n7.remove customer manually\n8.remove all customer \n0.exit \nenter choice: "))
     if meth == 0:
         break
+    elif meth == 1:
+        floors = int(input("How many floors?: "))
+        rooms = int(input("How many rooms?: "))
+        curbill, prebill = get_bills(init_txt, new_txt, floors, rooms)
+        water_tab, elec_tab, roomc_tab, total_tab = curbill.get_calculated_costs(prebill)
+        custable = curbill.get_name_table()
 
-    if meth == 1:
-        floors = int(input("how many floors?: "))
-        room = int(input("how many rooms per floor?: "))
     elif meth == 2:
         print("Please insert text file in Rent_Calculator folder")
-        initmon_yr = input("enter initial month text name(month_year ex. 11_21): ")
-        newmon_yr = input("enter current month text name(month_year ex. 12_21): ")
-        init_txt = f""
-
+        nmon, nyr = int(input("Enter billing month(number only ex. 12): ")), int(input("year (number ex. 2021): "))
+        init_txt = f"{nmon - 1}_{nyr}.txt"
+        new_txt = f"{nmon}_{nyr}.txt"
+        curbill, prebill = get_bills(init_txt, new_txt, floors, rooms)
+        water_tab, elec_tab, roomc_tab, total_tab = curbill.get_calculated_costs(prebill)
+        custable = curbill.get_name_table()
 
     elif meth == 3:
-        operate(init_txt,new_txt)
+        draw_table(total_tab,floors,rooms)
+    elif meth == 4:
+        draw_table(custable,floors,rooms)
+    elif meth == 5:
+        rfloor = int(input("What floor? : "))-1
+        rroom = int(input("What room? : "))-1
+        print(f"Customer name : {custable[rfloor][rroom]}")
+        print(f"Electric cost = {water_tab[rfloor][rroom]}")
+        print(f"Electric cost = {elec_tab[rfloor][rroom]}")
+        print(f"Room cost =  {roomc_tab[rfloor][rroom]}")
+        print(f"Total cost = {total_tab[rfloor][rroom]}")
+
+    elif meth == 6:
+        name = input("What's customer's name? : ")
+        rfloor = int(input("What floor? : "))
+        rroom = int(input("What room? : "))
+        cost = int(input("Room cost? : "))
+        initwater = int(input("init water? : "))
+        initelec = int(input("init electric? : "))
+        water = int(input("current water? : "))
+        elec = int(input("current electric? : "))
+        curbill.add_customer(rfloor,rroom,name,cost,water,elec)
+        prebill.add_customer(rfloor,rroom,name,cost,initwater,initelec)
+        print(f"Added {name}")
+        water_tab, elec_tab, roomc_tab, total_tab = curbill.get_calculated_costs(prebill)
+        custable = curbill.get_name_table()
+
+    elif meth == 7:
+        name = input("What's customer's name? : ")
+        # custable = curbill.remove_customer(custable,name)
+        curbill.remove_customer(name)
+        prebill.remove_customer(name)
+        print(f"Removed {name}")
+        water_tab, elec_tab, roomc_tab, total_tab = curbill.get_calculated_costs(prebill)
+        custable = curbill.get_name_table()
+
+    elif meth == 8:
+        for i in range(3):
+            curbill.remove_all()
+            prebill.remove_all()
+
+        water_tab, elec_tab, roomc_tab, total_tab = curbill.get_calculated_costs(prebill)
+        custable = curbill.get_name_table()
 
     else:
         print("incorrect answer try again")
+    print("")
 
-print("closed")
+
+print("\nclosed")
