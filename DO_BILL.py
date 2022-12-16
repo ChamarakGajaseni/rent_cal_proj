f3b930c9f6a2b8af3eccf9ad49d1bf624368a0a2
ChamarakGajaseni <chamarak.g@ku.th>
2022-12-16 11:51:25 +0700

    fixed some mistake

--- DO_BILL.py
+++ DO_BILL.py
@@ -176,73 +176,73 @@ print("")
 # turtle.onscreenclick(operate)
 # screen.mainloop()
 
-# while True:
-#     meth = int(input("1.change size \n2.change datas \n3.show all room rent \n4.show all customer \n5.show specific room detail \n6.add customer manually\n7.remove customer manually\n8.remove all customer \n0.exit \nenter choice: "))
-#     if meth == 0:
-#         break
-#     elif meth == 1:
-#         floors = int(input("How many floors?: "))
-#         rooms = int(input("How many rooms?: "))
-#         curbill, prebill = get_bills(init_txt, new_txt, floors, rooms)
-#         water_tab, elec_tab, roomc_tab, total_tab = curbill.get_calculated_costs(prebill)
-#         custable = curbill.get_name_table()
-#
-#     elif meth == 2:
-#         print("Please insert text file in Rent_Calculator folder")
-#         nmon, nyr = int(input("Enter billing month(number only ex. 12): ")), int(input("year (number ex. 2021): "))
-#         init_txt = f"{nmon - 1}_{nyr}.txt"
-#         new_txt = f"{nmon}_{nyr}.txt"
-#         curbill, prebill = get_bills(init_txt, new_txt, floors, rooms)
-#         water_tab, elec_tab, roomc_tab, total_tab = curbill.get_calculated_costs(prebill)
-#         custable = curbill.get_name_table()
-#
-#     elif meth == 3:
-#         draw_table(total_tab,floors,rooms)
-#     elif meth == 4:
-#         draw_table(custable,floors,rooms)
-#     elif meth == 5:
-#         rfloor = int(input("What floor? : "))-1
-#         rroom = int(input("What room? : "))-1
-#         print(f"Customer name : {custable[rfloor][rroom]}")
-#         print(f"Electric cost = {water_tab[rfloor][rroom]}")
-#         print(f"Electric cost = {elec_tab[rfloor][rroom]}")
-#         print(f"Room cost =  {roomc_tab[rfloor][rroom]}")
-#         print(f"Total cost = {total_tab[rfloor][rroom]}")
-#
-#     elif meth == 6:
-#         name = input("What's customer's name? : ")
-#         rfloor = int(input("What floor? : "))
-#         rroom = int(input("What room? : "))
-#         cost = int(input("Room cost? : "))
-#         initwater = int(input("init water? : "))
-#         initelec = int(input("init electric? : "))
-#         water = int(input("current water? : "))
-#         elec = int(input("current electric? : "))
-#         curbill.add_customer(rfloor,rroom,name,cost,water,elec)
-#         prebill.add_customer(rfloor,rroom,name,cost,initwater,initelec)
-#         print(f"Added {name}")
-#         water_tab, elec_tab, roomc_tab, total_tab = curbill.get_calculated_costs(prebill)
-#         custable = curbill.get_name_table()
-#
-#     elif meth == 7:
-#         name = input("What's customer's name? : ")
-#         curbill.remove_customer(name)
-#         prebill.remove_customer(name)
-#         print(f"Removed {name}")
-#         water_tab, elec_tab, roomc_tab, total_tab = curbill.get_calculated_costs(prebill)
-#         custable = curbill.get_name_table()
-#
-#     elif meth == 8:
-#         for i in range(floors):
-#             curbill.remove_all()
-#             prebill.remove_all()
-#
-#         water_tab, elec_tab, roomc_tab, total_tab = curbill.get_calculated_costs(prebill)
-#         custable = curbill.get_name_table()
-#
-#     else:
-#         print("incorrect answer try again")
-#     print("")
+while True:
+    meth = int(input("1.change size \n2.change datas \n3.show all room rent \n4.show all customer \n5.show specific room detail \n6.add customer manually\n7.remove customer manually\n8.remove all customer \n0.exit \nenter choice: "))
+    if meth == 0:
+        break
+    elif meth == 1:
+        floors = int(input("How many floors?: "))
+        rooms = int(input("How many rooms?: "))
+        curbill, prebill = get_bills(init_txt, new_txt, floors, rooms)
+        water_tab, elec_tab, roomc_tab, total_tab = curbill.get_calculated_costs(prebill)
+        custable = curbill.get_name_table()
+
+    elif meth == 2:
+        print("Please insert text file in Rent_Calculator folder")
+        nmon, nyr = int(input("Enter billing month(number only ex. 12): ")), int(input("year (number ex. 2021): "))
+        init_txt = f"{nmon - 1}_{nyr}.txt"
+        new_txt = f"{nmon}_{nyr}.txt"
+        curbill, prebill = get_bills(init_txt, new_txt, floors, rooms)
+        water_tab, elec_tab, roomc_tab, total_tab = curbill.get_calculated_costs(prebill)
+        custable = curbill.get_name_table()
+
+    elif meth == 3:
+        draw_table(total_tab,floors,rooms)
+    elif meth == 4:
+        draw_table(custable,floors,rooms)
+    elif meth == 5:
+        rfloor = int(input("What floor? : "))-1
+        rroom = int(input("What room? : "))-1
+        print(f"Customer name : {custable[rfloor][rroom]}")
+        print(f"Electric cost = {water_tab[rfloor][rroom]}")
+        print(f"Electric cost = {elec_tab[rfloor][rroom]}")
+        print(f"Room cost =  {roomc_tab[rfloor][rroom]}")
+        print(f"Total cost = {total_tab[rfloor][rroom]}")
+
+    elif meth == 6:
+        name = input("What's customer's name? : ")
+        rfloor = int(input("What floor? : "))
+        rroom = int(input("What room? : "))
+        cost = int(input("Room cost? : "))
+        initwater = int(input("init water? : "))
+        initelec = int(input("init electric? : "))
+        water = int(input("current water? : "))
+        elec = int(input("current electric? : "))
+        curbill.add_customer(rfloor,rroom,name,cost,water,elec)
+        prebill.add_customer(rfloor,rroom,name,cost,initwater,initelec)
+        print(f"Added {name}")
+        water_tab, elec_tab, roomc_tab, total_tab = curbill.get_calculated_costs(prebill)
+        custable = curbill.get_name_table()
+
+    elif meth == 7:
+        name = input("What's customer's name? : ")
+        curbill.remove_customer(name)
+        prebill.remove_customer(name)
+        print(f"Removed {name}")
+        water_tab, elec_tab, roomc_tab, total_tab = curbill.get_calculated_costs(prebill)
+        custable = curbill.get_name_table()
+
+    elif meth == 8:
+        for i in range(floors):
+            curbill.remove_all()
+            prebill.remove_all()
+
+        water_tab, elec_tab, roomc_tab, total_tab = curbill.get_calculated_costs(prebill)
+        custable = curbill.get_name_table()
+
+    else:
+        print("incorrect answer try again")
+    print("")
 
 
 
